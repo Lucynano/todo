43a2b6cd3126214a9ff7d3884660f5939de367ea
Lucynano <luciannorandrasaniaina@gmail.com>
2025-08-31 21:39:31 +0300

    crud

--- src/components/TODOList.jsx
+++ src/components/TODOList.jsx
@@ -1,67 +1,143 @@
-function Item({ item }) {
+import React from "react";
+
+function Item({ item, setTodos }) {
+    const completeTodo = () => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) => 
+                todo.id === item.id
+                    ? { ...todo, is_completed: !todo.is_completed }
+                    : todo
+            )
+        );
+    };
+
+    // edit
+    const [editing, setEditing] = React.useState(false);
+
+    const inputRef = React.useRef(null);
+    
+
+    const handleEdit = () => {
+        setEditing(true);
+    };
+
+    React.useEffect(() => {
+        if (editing && inputRef.current) {
+            inputRef.current.focus();
+
+            // position the cursor at the end of the text
+            inputRef.current.setSelectionRange(
+                inputRef.current.value.length,
+                inputRef.current.value.length
+            );
+        }
+    }, [editing]);
+
+    const handleInputSubmit = (event) => {
+        event.preventDefault();
+        setEditing(false);
+    };
+
+    const handleInputBlur = () => {
+        setEditing(false);
+    };
+
+    const handleInputChange = (e) => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) =>
+                todo.id === item.id ? { ...todo, title: e.target.value } : todo
+            )
+        );
+    };
+
+    // delete
+    const handleDelete = () => {
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== item.id));
+    };
+
     return (
         <li id={item?.id} className="todo_item">
-            <button className="todo_items_left">
-                <svg 
-                    height="100" 
-                    width="100" 
-                    xmlns="http://www.w3.org/2000/svg">
-                    <circle 
-                        cx="24"
-                        cy="48"
-                        r="18"
-                        stroke="limegreen"
-                        strokeWidth="2"
-                        fill="none"
-                    />
-                </svg> 
-                <p>{item?.title}</p>
-            </button>
-            <div className="todo_items_right">
-                <button>
-                    <span className="visually-hidden">Edit</span>
-                        <svg 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            fill="none" 
-                            viewBox="0 0 24 24" 
-                            strokeWidth="1.5" 
-                            stroke="#c2b39a" 
-                            width="30px" 
-                            height="30px">
-                            <path 
-                                strokeLinecap="round" 
-                                strokeLinejoin="round" 
-                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" 
-                            />
-                        </svg>
-                </button>
-                <button>
-                    <span className="visually-hidden">Delete</span>
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        fill="none" 
-                        viewBox="0 0 24 24" 
-                        strokeWidth="1.5" 
-                        stroke="#c2b39a" 
-                        width="30px" 
-                        height="30px">
-                        <path 
-                            strokeLinecap="round" 
-                            strokeLinejoin="round" 
-                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" 
+            {editing ? (
+                <form className="edit-form" onSubmit={handleInputSubmit}>
+                    <label htmlFor="edit-todo">
+                        <input
+                            ref={inputRef}
+                            type="text"
+                            name="edit-todo"
+                            id="edit-todo"
+                            defaultValue={item?.title}
+                            onBlur={handleInputBlur}
+                            onChange={handleInputChange}
                         />
-                    </svg>
-                </button>
-            </div>
+                    </label>
+                </form>
+            ) : (
+                <>
+                    <button className="todo_items_left" onClick={completeTodo}>
+                        <svg 
+                            height="100" 
+                            width="100" 
+                            xmlns="http://www.w3.org/2000/svg">
+                            <circle 
+                                cx="24"
+                                cy="48"
+                                r="18"
+                                stroke="#22C55E"
+                                strokeWidth="2"
+                                fill={item.is_completed ? "#22C55E" : "#0d0d0d"}
+                            />
+                        </svg> 
+                        <p style={item.is_completed ? {textDecoration: "line-through"} : {}}>
+                            {item?.title}
+                        </p>
+                    </button>
+                    <div className="todo_items_right">
+                        <button onClick={handleEdit}>
+                            <span className="visually-hidden">Edit</span>
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="none" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="1.5" 
+                                stroke="#c2b39a" 
+                                width="30px" 
+                                height="30px">
+                                <path 
+                                    strokeLinecap="round" 
+                                    strokeLinejoin="round" 
+                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" 
+                                />
+                            </svg>
+                        </button>
+                        <button onClick={handleDelete}>
+                            <span className="visually-hidden">Delete</span>
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="none" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="1.5" 
+                                stroke="#c2b39a" 
+                                width="30px" 
+                                height="30px">
+                                <path 
+                                    strokeLinecap="round" 
+                                    strokeLinejoin="round" 
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" 
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                </>
+            )}
         </li>
     );
 }
 
-function TODOList({ todos }) {
+function TODOList({ todos, setTodos }) {
     return (
         <ol className="todo_list">
             {todos && todos.length > 0 ? (
-                todos?.map((item, index) => <Item key={index} item={item}/>)
+                todos?.map((item, index) => <Item key={index} item={item} setTodos={setTodos} />)
             ) : (
                 <p>Seems lonely in here, what are you up to?</p>
             )}
